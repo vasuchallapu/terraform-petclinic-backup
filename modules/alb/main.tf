36955d6922a2e06845058f7cb9680feb5a8414b9
vasuchallapu <vasuchallapu@gmail.com>
2024-09-25 23:16:03 +0530

    ssl certificate added

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -49,7 +49,8 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.public_lb.arn
   port              = 443
   protocol          = "HTTPS"
-
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:588738567172:certificate/09a44862-6f9e-4176-96b7-bcddb5e099e4" # Replace with your certificate ARN
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
