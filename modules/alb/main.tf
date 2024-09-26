d1922038e5e6987cf9db029d40db2c114c12b26f
vasuchallapu <vasuchallapu@gmail.com>
2024-09-26 10:57:52 +0530

    acm certificate arn call dynamically

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -44,13 +44,20 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+# Fetch the ACM certificate ARN dynamically based on domain name
+data "aws_acm_certificate" "petclinic_cert" {
+  domain = "vasuchallapu.click"
+  most_recent = true
+  statuses = ["ISSUED"]
+}
+
 # Listener for HTTPS Traffic
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.public_lb.arn
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:acm:us-east-1:588738567172:certificate/09a44862-6f9e-4176-96b7-bcddb5e099e4" # Replace with your certificate ARN
+  certificate_arn   = data.aws_acm_certificate.petclinic_cert.arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
