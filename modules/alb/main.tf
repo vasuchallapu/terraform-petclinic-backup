0e55bab5ae45244a3fd00fb5f23bf00954b02d8d
vasuchallapu <vasuchallapu@gmail.com>
2024-09-28 15:58:54 +0530

    ACM fix

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -51,12 +51,11 @@ resource "aws_lb_listener" "http" {
 
 # Listener for HTTPS Traffic
 resource "aws_lb_listener" "https" {
-  source            = "../../modules/alb"
   load_balancer_arn = aws_lb.public_lb.arn
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cert.arn
+  certificate_arn   = "aws_acm_certificate.cert.arn"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
